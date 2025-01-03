f680ec29784450cfd6abc45fb6214c177e194317
harshshah181205 <harshshah123346@gmail.com>
2025-01-03 14:37:11 +0530

    minor bugs fixed

--- Backend/server.js
+++ Backend/server.js
@@ -13,7 +13,9 @@ app.use(cors({
   origin: 'http://localhost:3000',
   credentials: true
 }));
+
 dotenv.config();
+
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
